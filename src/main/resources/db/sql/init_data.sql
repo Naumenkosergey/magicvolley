2cc7d065c577573d02ceaab517811b2ecdc920d9
sergey <naumenkosergeyns@gmail.com>
2024-11-27 23:11:52 +0300

    images path paxar

--- src/main/resources/db/sql/init_data.sql
+++ src/main/resources/db/sql/init_data.sql
@@ -23,7 +23,7 @@ values('1e6d19c6-4594-4d5f-8578-0a0479cd6caa', 'e5050cc4-98c4-4b48-9e47-8b5057c6
 
 insert into media_storages(id, entity_id, content_type, data, file_name, size, type_entity, version)
 values('8651fdf6-6f7b-482d-97eb-4d9c7165e8a0', 'dd5dc5ea-f858-47c0-b518-be2d1565856d',
-'image/png', pg_read_binary_file('/Users/user/IdeaProjects/magicvolley/src/main/resources/static/image.png')::bytea,
+'image/png', pg_read_binary_file('D:\KODING\magicvolley\src\main\resources\static\image.png')::bytea,
 'image.png', 547840, 'CAMP', 0);
 
 insert into media_storages(id, entity_id, content_type, data, file_name, size, type_entity, version)
