e0eeaf38160372f3bd6b0203c39c6aea072826cb
sergey <naumenkosergeyns@gmail.com>
2024-12-20 20:36:50 +0300

    test package

--- src/test/java/ru/magicvolley/DemoMvApplicationTests.java
+++ src/test/java/ru/magicvolley/DemoMvApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.magicvolley;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoMvApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
