ba86f34f74cfd2203fca8f962f1f648f4e3e3d2c
sergey <naumenkosergeyns@gmail.com>
2025-03-25 21:46:06 +0300

    changeset

--- src/main/resources/db/sql/init_ddl.sql
+++ src/main/resources/db/sql/init_ddl.sql
@@ -219,7 +219,7 @@ create table if not exists about_us_page (
 
 create table if not exists reviews (
     id uuid primary key not null,
-    review_text text,
+    review_text varchar(400),
     date_review date,
     name_reviewer varchar(255),
     image_id uuid references media_storages (id),
