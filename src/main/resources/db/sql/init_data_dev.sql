26aa25be8071986ef2aa9b96ed803deccf948a44
sergey <naumenkosergeyns@gmail.com>
2025-05-16 23:25:34 +0300

    develop init data for user admin username change telephone

--- src/main/resources/db/sql/init_data_dev.sql
+++ src/main/resources/db/sql/init_data_dev.sql
@@ -7,10 +7,10 @@ values('888f4834-c6e4-472e-808b-171febbb9137', 'USER', 0);
 
 
 insert into users(id, telephone, username, password, email,is_blocked, role_id, version)
-values ('68c8432c-b963-46c2-b1ff-345452609ecf','admin', 'Казина Татьяна', '$2y$10$6lSrg0Ao0g8H1x7mp7bCRertVWA.thsfgIQY.TyNvpp6d51lWp9za',
+values ('68c8432c-b963-46c2-b1ff-345452609ecf','79969103047', 'Казина Татьяна', '$2y$10$6lSrg0Ao0g8H1x7mp7bCRertVWA.thsfgIQY.TyNvpp6d51lWp9za',
 'kazina@mail.ru', false,'1ffdc9e5-ece0-4420-ba12-b4ac1ed25573', 0);
 insert into users(id, telephone, username, password, email, is_blocked, role_id, version)
-values ('a8fd2366-51d0-47d0-a06b-819f41af4cb8','1', 'Науменко Сергей', '$2y$10$OvF1JVol9zuT92MoqnwFceMle3hr88.dL6oe6Eb0ioAD434R1/BiG',
+values ('a8fd2366-51d0-47d0-a06b-819f41af4cb8','79998620452', 'Науменко Сергей', '$2y$10$OvF1JVol9zuT92MoqnwFceMle3hr88.dL6oe6Eb0ioAD434R1/BiG',
 'naumeko@mail.ru', false, '888f4834-c6e4-472e-808b-171febbb9137', 0);
 insert into users(id, telephone, username, password, email, is_blocked, role_id, version)
 values ('e5050cc4-98c4-4b48-9e47-8b5057c6b46c','marozova', 'marozova natalia', '$2y$10$eFbYvZne/FFfGUlvJfyqzuWTAj9Hq/7gta/uQAm0ZW9nKw.XFypOu',
@@ -153,11 +153,11 @@ insert into camp_coaches(camp_id, coach_id, version) values ('7573f07f-3769-4f34
 
 insert into user_profile (user_id, image_id, ful_name, telephone, birthday, version)
 values ('a8fd2366-51d0-47d0-a06b-819f41af4cb8', '5751fdf6-6f7b-482d-97eb-4d9c7165e8a0', 'Науменко Сергей',
-'+7998620452','1993-03-24', 0);
+'79998620452','1993-03-24', 0);
 
 insert into user_profile (user_id, image_id, ful_name, telephone, birthday, version)
 values ('68c8432c-b963-46c2-b1ff-345452609ecf', '2d8f2bc3-6bf8-5b64-b5e9-8071296a0f12', 'Казина Татьяна',
-'+79969103047', null, 0);
+'79969103047', null, 0);
 
 insert into profile_camps (profile_id, camp_id, is_past, is_booked, version)
 values ('a8fd2366-51d0-47d0-a06b-819f41af4cb8', 'dd5dc5ea-f858-47c0-b518-be2d1565856d', true, false, 0);
